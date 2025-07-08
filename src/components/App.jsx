b7a58145bed29a9ea4b2d98d776379324fa1b97a
Juan Torres <juan93git@gmail.com>
2025-07-08 11:40:00 +0200

    Button component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
 import BookSummary from "./BookSummary";
+import Button from "./Button";
 
 function App() {
   return (
     <div>
       <BookSummary />
+      <Button>Button</Button>
+      <Button type="primary" onClick={() => alert("hey!")}>
+        Button
+      </Button>
     </div>
   );
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,20 @@
+import styles from "./Button.module.css";
+
+function Button({ children, onClick, type }) {
+  // type can be: "primary"
+
+  const handleClick = () => {
+    return onClick ? onClick() : () => {};
+  };
+
+  return (
+    <button
+      className={`${styles.btn} ${type ? styles[type] : ""}`}
+      onClick={handleClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
